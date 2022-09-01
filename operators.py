48ff7bfdbf2d8dfe39cf619b88b9de14d63a2a17
amerhussain93 <111168188+amerhussain93@users.noreply.github.com>
2022-09-01 14:30:25 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,174 @@
+print("============================ addition =============================")
+x = 10
+y = 20
+print("Result is :",x+y)
+
+x = 28
+y = 29
+z = x+y
+print("the sum given intiger is ", z)
+
+n1 = int(input("enter the +ve intiger: "))
+n2 = int(input("enter the +ve integer: "))
+n  = n1+n2
+print("the sum of given number is : ",n)
+
+a = int(input("Enter a number "))
+b = int(input("Enter the number"))
+c = a+b
+print("addition of the given numbers is :- ",c)
+
+odd1 = int(input(" enter the odd number "))
+odd2 = int(input(" enter the odd number"))
+odd = odd1+odd2
+print("the sum of the given odd number is :-",odd)
+
+print("================ subtraction ================")
+
+x = 20
+y = 15
+print("Result is :",x-y)
+
+x = 38
+y = 20
+z = x-y
+print("the sum given intiger is ", z)
+
+n1 = int(input("enter the +ve intiger: "))  # (n1 should be greater than n2)
+n2 = int(input("enter the +ve intiger: "))
+n = n1-n2
+print("the subs of given number is : ",n)
+
+a = int(input("Enter a number "))           # (a should be greater than b)
+b = int(input("Enter the number"))
+c = a-b
+print("subtraction of the given numbers is :- ",c)
+
+even1 = int(input(" enter the odd number "))
+even2 = int(input(" enter the odd number"))
+even = even1-even2
+print("the sub of the given even number is :-",even)  # (even1 should be greater than even2)
+
+print("================ division ================")
+
+x = 20
+y = 5
+print("Result is :", x/y)
+
+x = 20
+y = 4
+print("Result is :", x % y)
+
+x = 38
+y = 20
+z = x/y
+print("the division of  given integer is ", z)
+
+x = 38
+y = 20
+z = x%y
+print("the modulus of given integer is ", z)
+
+x = 38
+y = 20
+z = x/y
+print("the sum given integer is ", z)
+
+
+n1 = int(input("enter the +ve integer: "))  # (n1 should be greater than n2)
+n2 = int(input("enter the +ve integer: "))
+n = n1/n2
+print("the division of given number is : ", n)
+
+a = int(input("Enter a number "))           # (a should be greater than b)
+b = int(input("Enter the number"))          #  ( % it gives the reminder value of the division)
+c = a % b
+print("subtraction of the given numbers is :- ",c)
+
+even1 = int(input(" enter the odd number "))
+even2 = int(input(" enter the odd number"))
+even = even1//even2
+print("the floor division of the given even number is :-", even)  # (even1 should be greater than even2)
+
+print("================ multiplication ================")
+
+x = 20
+y = 9
+print("Result is :", x*y)
+
+x = 20
+y = 8
+print("Result is :", x * y)
+
+x = 38
+y = 5
+z = x*y
+print("the product of  given integer is ", z)
+
+x = 38
+y = 2
+z = x*y
+print("the multiplication of given integer is ", z)
+
+x = 38
+y = 20
+z = x*y
+print("the product given integer is ", z)
+
+
+n1 = int(input("enter the +ve integer: "))
+n2 = int(input("enter the +ve integer: "))
+n = n1*n2
+print("the product of given number is : ", n)
+
+a = int(input("Enter a number "))
+b = int(input("Enter the number"))
+c = a * b
+print("product of the given numbers is :- ",c)
+
+even1 = int(input(" enter the odd number "))
+even2 = int(input(" enter the odd number"))
+even = even1*even2
+print("the floor product of the given even number is :-", even)
+
+print("================ exponential ================")
+
+x = 20
+y = 9
+print("Result is :", x**y)
+
+x = 20
+y = 8
+print("Result is :", x ** y)
+
+x = 38
+y = 5
+z = x**y
+print("the power of  x is ", z)
+
+x = 38
+y = 2
+z = x**y
+print("the power of given integer is ", z)
+
+x = 38
+y = 20
+z = x**y
+print("the product given integer is ", z)
+
+
+n1 = int(input("enter the +ve integer: "))
+n2 = int(input("enter the +ve integer: "))
+n = n1**n2
+print("the power of given number is : ", n)
+
+a = int(input("Enter a number "))
+b = int(input("Enter the number"))
+c = a ** b
+print("power of the given numbers is :- ",c)
+
+even1 = int(input(" enter the odd number "))
+even2 = int(input(" enter the odd number"))
+even = even1**even2
+print("the power of the given even number is :-", even)
+
